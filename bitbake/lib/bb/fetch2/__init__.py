9ff3564b9e22d022c7cfac2abf97571953a78885
Christopher Larson <chris_larson@mentor.com>
2015-08-10 14:03:40 -0700

    bitbake: bb.fetch: simplify mirror tarball handling in try_mirror_url
    Rather than checking mirrortarball explicitly, simply check and see if the
    localpath basenames match up.
    
    We can't just compare methods, as in the case where an http file is found via
    a file mirror, that causes the http fetcher to be run to process the
    nonexistent mirror tarball, resulting in an attempt to download from upstream
    when we already have the file.
    
    This will make it easier to add the ability to support multiple mirror
    tarballs, to implement shallow git mirror tarball support.
    
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- bitbake/lib/bb/fetch2/__init__.py
+++ bitbake/lib/bb/fetch2/__init__.py
@@ -938,8 +938,7 @@ def try_mirror_url(fetch, origud, ud, ld, check = False):
         # We may be obtaining a mirror tarball which needs further processing by the real fetcher
         # If that tarball is a local file:// we need to provide a symlink to it
         dldir = ld.getVar("DL_DIR", True)
-        if origud.mirrortarball and os.path.basename(ud.localpath) == os.path.basename(origud.mirrortarball) \
-                and os.path.basename(ud.localpath) != os.path.basename(origud.localpath):
+        if os.path.basename(ud.localpath) != os.path.basename(origud.localpath):
             # Create donestamp in old format to avoid triggering a re-download
             if ud.donestamp:
                 bb.utils.mkdirhier(os.path.dirname(ud.donestamp))
@@ -949,7 +948,7 @@ def try_mirror_url(fetch, origud, ud, ld, check = False):
                 os.symlink(ud.localpath, dest)
             if not verify_donestamp(origud, ld) or origud.method.need_update(origud, ld):
                 origud.method.download(origud, ld)
-                if hasattr(origud.method,"build_mirror_data"):
+                if hasattr(origud.method, "build_mirror_data"):
                     origud.method.build_mirror_data(origud, ld)
             return origud.localpath
         # Otherwise the result is a local file:// and we symlink to it
